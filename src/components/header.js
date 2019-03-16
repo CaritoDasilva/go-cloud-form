143b3cd3942756b1a02db1de30d1a5ccf74646a8
CaritoDasilvagit <caritodasilva@gmail.com>
2019-03-16 00:45:33 -0300

    se agrega imagen al header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "../imgs/logo.png";
 
 const Header = props => {
   return (
     <div>
-      <div class="card-body">This is some text within a card body.</div>
+      <div class="card-body">
+        <img src={logo} alt="application logo" />
+      </div>
     </div>
   );
 };
